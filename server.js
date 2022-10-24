34e8fdc52f8ae039a496689c4c328442ff91d929
topdev107 <94303973+topdev511@users.noreply.github.com>
2022-10-24 09:57:42 +0900

    test

--- server.js
+++ server.js
@@ -16,15 +16,14 @@ connectDB();
 
 // cors
 // app.use(cors({ origin: true, credentials: true }));
-app.use(cors());
 // app.use(cors({origin:['https://presale-backend.vercel.app', 'https://flash-launch.com']}));
-// var corsOptions = {
-//   origin: 'https://presale-backend.vercel.app',
-//   optionsSuccessStatus: 200, // For legacy browser support
-//   methods: "GET, POST"
-// }
+var corsOptions = {
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Init Middleware
 app.use(express.json());
